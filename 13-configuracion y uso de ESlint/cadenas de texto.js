eb3df6e6807602e2f82d1dec725f80bebb38dff4
Fran <99873754+Franciscogarciaguerrero@users.noreply.github.com>
2022-09-02 13:17:53 +0200

    Add files via upload

---
+++ 13-configuracion y uso de ESlint/cadenas de texto.js
@@ -0,0 +1,28 @@
+const nombre = "Francisco"
+const apellido = "Garcia"
+//const backticks = "backticks"
+const estudiante = nombre.concat(" ").concat(apellido)
+
+console.log(estudiante)
+
+const estudianteMayus = estudiante.toUpperCase()
+const estudianteMinus = estudiante.toLowerCase()
+
+console.log(estudianteMayus)
+console.log(estudianteMinus)
+
+const tamañoCadena = estudiante.length
+console.log(tamañoCadena)
+
+const primeraLetra = nombre.slice(0, 1)
+console.log(primeraLetra)
+
+const ultimaLetra = apellido.slice(-1)
+console.log(ultimaLetra)
+
+const estudianteSinEspacios = estudiante.replace(" ", "")
+console.log(estudianteSinEspacios)
+
+const nombreIncluido = estudiante.includes(nombre)
+console.log(nombreIncluido)
+
