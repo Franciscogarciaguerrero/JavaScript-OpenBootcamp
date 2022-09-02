6f5c7bb3ffc26296e031661ad2ec635daea1819b
Fran <99873754+Franciscogarciaguerrero@users.noreply.github.com>
2022-09-02 13:12:10 +0200

    Add files via upload

---
+++ 10-modulos/index.js
@@ -0,0 +1,7 @@
+import * as moduloMatematicas from './controller.js'
+
+import chalk from 'chalk';
+
+console.log(chalk.green(moduloMatematicas.suma(4, 4)))
+
+console.log(chalk.green(moduloMatematicas.multiplica(2, 2)))
